5c41bb693f939da5ff072e9936aa534a77f9221c
keith <1>
2024-04-10 18:05:41 +0800

    1

--- start_rmbg.bat
+++ start_rmbg.bat
@@ -1,2 +1,4 @@
-call .venv\Scripts\activate.ps1
+python -m venv .venv
+call .venv\Scripts\activate.bat
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 python start_rmbg.py
